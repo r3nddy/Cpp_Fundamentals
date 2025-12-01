5b01ceebe892ebc6d5a091a70df93376f3ca2358
Rendy <rendyexpayer05@gmail.com>
2025-12-01 21:28:36 +0800

    first commit

---
+++ kostanliteral.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// Literal adalah nilai langsung yang
+// kamu tulis di dalam kode program,
+// bukan diambil dari variabel.
+
+int main() {
+    std::cout << 200 << std::endl;
+    std::cout << 3.14159 << std::endl;
+    std:: cout << 'R' << std::endl;
+    std:: cout << "C++ Dibuat oleh Bjarne Stroustrup" << std::endl;
+    std:: cout << true << std::endl;
+    
+    return 0;
+}
