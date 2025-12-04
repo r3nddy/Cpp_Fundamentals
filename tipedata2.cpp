b16eb15678f8a8fac83b92c9b355529af08564dc
Rendy <rendyexpayer05@gmail.com>
2025-12-04 21:57:51 +0800

    const, unsigned

--- tipedata2.cpp
+++ tipedata2.cpp
@@ -3,6 +3,8 @@
 
 // char wajib ' ' karena hanya untuk 1 karakter
 // " " selalu dianggap string literal, karena di akhir selalu ada null terminator \0
+// unsigned = tidak ada tanda negatif.
+//  keyword const digunakan untuk membuat nilai tidak bisa diubah setelah dideklarasi.
 
 int main() {
     float velocity = 10.4;
@@ -11,5 +13,10 @@ int main() {
     std::string pesan = "Rendy lagi main Valorant";
     std::cout << pesan << std::endl;
     std::cout << "nilai dari variabel logic : " << logic << std::endl;
+
+
+    unsigned int panjang = 40;
+    const float gravitasi = 9.8;
+    std::cout << gravitasi;
     return 0;
 }
