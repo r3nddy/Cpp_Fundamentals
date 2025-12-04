3b4c8ff8d4acd948bff5d01ad4c3b7104269c172
Rendy <rendyexpayer05@gmail.com>
2025-12-04 08:16:57 +0800

    tipe data integer

---
+++ tipedata.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// tipe data int
+
+int main() {
+    int dmg = 20;
+    std::cout << dmg << std::endl;
+    std::cout << "damage player adalah " << dmg << " ATK Damage" << std::endl;
+    return 0;
+}
